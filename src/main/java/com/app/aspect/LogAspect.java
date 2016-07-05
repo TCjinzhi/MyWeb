fae0d985004137d87859a4b64c4024decfe11a6f
Zhanghj <550727632@qq.com>
2016-07-05 11:06:49 +0800

    @around注解记录 每个方法的开始结束及异常信息

--- src/main/java/com/app/aspect/LogAspect.java
+++ src/main/java/com/app/aspect/LogAspect.java
@@ -1,5 +1,7 @@
 package com.app.aspect;
 
+import javax.validation.constraints.Null;
+
 import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -22,41 +24,46 @@ public class LogAspect {
 	public void pointCut() {
 	}
 
-	@After("pointCut()")
-	public void after(JoinPoint joinPoint) {
-		String clazz = joinPoint.getTarget().getClass().getName();
-		String methodName = joinPoint.getSignature().getName();
-		System.out.println("after aspect class:"+clazz+",method:"+ methodName+" executed");
-	}
+//	@After("pointCut()")
+//	public void after(JoinPoint joinPoint) {
+//		String clazz = joinPoint.getTarget().getClass().getName();
+//		String methodName = joinPoint.getSignature().getName();
+//		System.out.println("after aspect class:"+clazz+",method:"+ methodName+" executed");
+//	}
+//
+//	@Before("pointCut()")
+//	public void before(JoinPoint joinPoint) {
+//		//如果需要这里可以取出参数进行处理
+//		//Object[] args = joinPoint.getArgs();
+//		System.out.println("before aspect executing");
+//	}
 
-	@Before("pointCut()")
-	public void before(JoinPoint joinPoint) {
-		//如果需要这里可以取出参数进行处理
-		//Object[] args = joinPoint.getArgs();
-		System.out.println("before aspect executing");
-	}
-
-	@AfterReturning(pointcut = "pointCut()", returning = "returnVal")
-	public void afterReturning(JoinPoint joinPoint, Object returnVal) {
-		System.out.println("afterReturning executed, return result is "
-				+ returnVal);
-	}
+//	@AfterReturning(pointcut = "pointCut()", returning = "returnVal")
+//	public void afterReturning(JoinPoint joinPoint, Object returnVal) {
+//		System.out.println("afterReturning executed, return result is "
+//				+ returnVal);
+//	}
 
 	@Around("pointCut()")
-	public void around(ProceedingJoinPoint pjp) throws Throwable {
-		System.out.println("around start..");
+	public Object around(ProceedingJoinPoint pjp) throws Throwable {
+		Object result = null;
+		String clazz = pjp.getTarget().getClass().getName();
+		String methodName = pjp.getSignature().getName();
+		logger.info(clazz + "." + methodName + " start");
+		System.out.println(clazz + "." + methodName + " start");
 		try {
-			pjp.proceed();
+			result = pjp.proceed();
 		} catch (Throwable ex) {
-			System.out.println("error in around");
+			System.out.println("error in " + clazz + "." + methodName);
 			throw ex;
 		}
-		System.out.println("around end");
+		System.out.println(clazz + "." + methodName + " end");
+		return result;
 	}
-
-	@AfterThrowing(pointcut = "pointCut()", throwing = "error")
-	public void afterThrowing(JoinPoint jp, Throwable error) {
-		System.out.println("error:" + error);
-	}
+//
+//	@AfterThrowing(pointcut = "pointCut()", throwing = "error")
+//	public void afterThrowing(JoinPoint jp, Throwable error) {
+//		System.out.println("error:" + error);
+//	}
 
 }

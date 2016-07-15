b829d8a788c29b1f86bb0c0aabab16c5c3d85296
Zhanghj <550727632@qq.com>
2016-07-15 17:33:45 +0800

    获取api写入线程

--- src/main/java/com/app/api/giftListThread.java
+++ src/main/java/com/app/api/giftListThread.java
@@ -3,6 +3,10 @@ package com.app.api;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -13,12 +17,73 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 import com.alibaba.fastjson.JSON;
+import com.app.pojo.APIGift;
 import com.app.pojo.APIResponse;
 
 public class giftListThread implements Runnable {
 
 	@Override
 	public void run() {
+		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		String url = "jdbc:mysql://localhost/study";
+		String user = "root";
+		String password = "123456";
+		String sql = "insert into gifts (id,gift_name,gift_point,gift_image) values (?,?,?,?);";
+		
+		PreparedStatement checkps = null;
+		String checksql = "select * from gifts where id = ?";
+		
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(url, user, password);
+			preparedStatement = connection.prepareStatement(sql);
+			
+			checkps = connection.prepareStatement(checksql);
+			
+			APIResponse result = getGiftLists();
+			
+			for(APIGift data : result.getData()){
+				preparedStatement.setString(1, data.getId());
+				preparedStatement.setString(2, data.getGift_name());
+				preparedStatement.setString(3, data.getGift_point());
+				preparedStatement.setString(4, data.getGift_image());
+				
+				checkps.setString(1, data.getId());
+				
+				if(checkps.executeQuery().next()){
+					continue;
+				}
+					preparedStatement.execute();
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				preparedStatement.close();
+				checkps.close();
+				connection.close();
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		try {
+			Thread.sleep(1000*60*60*8);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
+	public static APIResponse getGiftLists(){
 		CloseableHttpClient client = HttpClients.createDefault();
 		URI uri = null;
 		try {
@@ -40,13 +105,14 @@ public class giftListThread implements Runnable {
 			response = client.execute(get);
 			  System.out.println(response);
 		      if(response.getStatusLine().getStatusCode()==200){
-		        System.out.println(EntityUtils.toString(response.getEntity()));
+//		        System.out.println(EntityUtils.toString(response.getEntity()));
 		        String string = EntityUtils.toString(response.getEntity());
-		        APIResponse apiResponse = JSON.parseObject(string,APIResponse.class);
-		        
-//		        System.out.println("apiResponse:"+apiResponse);
 //		        JSONObject obj = JSONObject.fromObject(string);
-		        
+		        APIResponse apiResponse = JSON.parseObject(string,APIResponse.class);
+		        System.out.println("apiResponse:"+apiResponse.getData().size());
+		        System.out.println(JSON.toJSONString(apiResponse, true));
+//		        System.out.println(obj.get("data").toString());
+		        return apiResponse;
 		        
 		      }
 		} catch (ClientProtocolException e) {
@@ -56,6 +122,10 @@ public class giftListThread implements Runnable {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	      
+		return null;
+	    
 	}
+	
 
 }

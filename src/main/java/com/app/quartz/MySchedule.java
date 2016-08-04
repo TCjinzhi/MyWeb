270d90c0a4451b6a8ef7b29783cc13995480f486
Zhanghj <550727632@qq.com>
2016-08-04 15:59:39 +0800

    根据spring 4.0 改写quartz配置 参考网址：http://www.cnblogs.com/Alandre/p/3733249.html

--- src/main/java/com/app/quartz/MySchedule.java
+++ src/main/java/com/app/quartz/MySchedule.java
@@ -1,10 +1,21 @@
 package com.app.quartz;
 
-public class MySchedule {
+import java.util.Date;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+public class MySchedule extends QuartzJobBean{
 
 	 public void printSomething(){
 	    //内容就是打印一句话
-	    System.out.println("this is test schedule");
+	    System.out.println("this is a test schedule at " + new Date());
 	  }
+
+	@Override
+	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+		this.printSomething();
+	}
 	
 }
